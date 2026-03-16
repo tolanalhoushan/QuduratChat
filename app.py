2f4389d2116ec27a9d4ce0b2b0adfeb0476426c0
tolanalhoushan <tolanalhoushan@gmail.com>
2026-03-16 05:59:03 +0300

    Update import statement for message classes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain_openai.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
-
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 st.set_page_config(page_title="مدرب القدرات الذكي", page_icon="🇸🇦")
 st.title("🇸🇦 مدرب اختبار القدرات (GAT)")
 
